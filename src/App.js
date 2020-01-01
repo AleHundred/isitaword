f77a32f05ba61647e58d8ec107752006eb5f7874
Alejandra Villa <codeme@alxvilla.me>
2020-01-01 19:26:17 +0000

    Use dictionaryapi.com

--- src/App.js
+++ src/App.js
@@ -2,63 +2,38 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Search from "./Search";
 
-const Dictionary = require("oxford-dictionary");
-const config = {
-  app_id: "1770efd9",
-  app_key: "b0f9fab04917158412b25a5de1490ba4",
-  source_lang: "en-us"
-};
-
-const dict = new Dictionary(config);
-console.log(dict);
-
-const OXFORD_DICTIONARY_API_URL =
-  " 	https://od-api.oxforddictionaries.com/api/v2";
+const API_KEY = "2f627ebc-9c50-44a7-bfc2-ba93f4721a09";
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [words, setWords] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
+  const [result, setResult] = useState(null);
 
   useEffect(() => {
-    fetch(OXFORD_DICTIONARY_API_URL)
+    fetch(
+      `https://dictionaryapi.com/api/v3/references/collegiate/json/${searchValue}?key=${API_KEY}`
+    )
       .then(response => response.json())
       .then(jsonResponse => {
-        setWords(jsonResponse.Search);
         setLoading(false);
+        if (typeof jsonResponse[0] === "object") {
+          setResult(`Yay! ${searchValue} IS a word!`);
+        } else {
+          setResult(`Oh noes! ${searchValue} is not a word!`);
+        }
       });
-  }, []);
+  }, [searchValue]);
 
   const search = searchValue => {
     setLoading(true);
-    setErrorMessage(null);
-    const lookup = dict.find(searchValue);
-    console.log("We are just searching now", searchValue);
-    lookup.then(
-      function(res) {
-        // stringify JSON object to see full structure in console log
-        console.log(JSON.stringify(res, null, 4));
-      },
-      function(err) {
-        console.log(err);
-      }
-    );
+    setSearchValue(searchValue);
   };
 
   return (
     <div className="App">
       <Search search={search} />
       <div className="words-info">
-        {loading && !errorMessage ? (
-          <span>loading...</span>
-        ) : errorMessage ? (
-          <div>{errorMessage}</div>
-        ) : (
-          words.map(
-            (word, index) => console.log("Word: ", word)
-            // add component for words
-          )
-        )}
+        {loading && !result ? <span>loading...</span> : <div>{result}</div>}
       </div>
     </div>
   );
